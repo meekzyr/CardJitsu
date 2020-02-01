ed279bf43f69f0015de0f9d4b85fcdfc0409781a
Bryan Muschter <bryanmuschter@gmail.com>
2020-02-01 02:49:54 -0500

    serverbase: fix taskMgr being hung up, add globalClock

--- game/jitsu/DistributedCardJitsuAI.py
+++ game/jitsu/DistributedCardJitsuAI.py
@@ -1,5 +1,4 @@
 from direct.distributed.DistributedNodeAI import DistributedNodeAI
-from direct.distributed.ClockDelta import *
 from direct.fsm import ClassicFSM, State
 from game.jitsu.CardJitsuGlobals import *
 from direct.directnotify.DirectNotifyGlobal import directNotify
